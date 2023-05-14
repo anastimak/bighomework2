81c15697aeb65bc07e57695bb0c91d7c3e7f4e57
anstice <apple@MacBook-Air.local>
2023-05-14 15:53:20 +0300

    создание utils

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer'
 import Main from './components/Main';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { LS_TOKEN } from './utils/constants';
 
 function App() {
   const [todos, setTodos] = useState(() => {
@@ -10,6 +12,43 @@ function App() {
 
     return todoList ? JSON.parse(todoList) : []
   })
+
+  // первый аргумент - колбек - ЧИСТАЯ функция
+  // массив зависимостей отсуствует - срабатывает на каждый рендер компонента
+  // массив зависимостей пустой - сработает 1 раз при инициализации
+  // массив зависимостей указан - сработает 1 раз при инициализации, 
+  //                              срабатывает при изменении значений в массиве
+  useEffect(() => {
+    localStorage.setItem(LS_TOKEN, JSON.stringify(todos))
+  }, [todos])
+
+  // не чистая функция
+  const exFunc = (number) => {
+    return number + Math.random()
+  }
+
+  // чистая функция
+  const exFunc4 = (number) => {
+    return number * 3
+  }
+
+  const addToList = (value) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: value,
+      status: false
+    }
+
+    setTodos((prev) => [newTodo, ...prev])
+  }
+
+  const deleteList = () => {
+    setTodos([])
+  }
+
+  const deleteOneTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
 
   console.log('APP render');
 
--- my-app/src/components/Footer/index.jsx
+++ my-app/src/components/Footer/index.jsx
@@ -1,4 +1,4 @@
-import {} from './footer.module.css'
+import styles from './footer.module.css'
 
 export const Footer = () => {
     console.log('Footer render');
