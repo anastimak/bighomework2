612a1f9d9aa23e7e8c8b41e5b35112afd0580a48
anstice <apple@MacBook-Air.local>
2023-05-16 17:26:48 +0300

    создание footer

--- my-app/src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './footer.module.css'
-
-export const Footer = () => {
-    console.log('Footer render');
-  
-    return (
-      <footer className={styles['wrapper-knjasd']}>Footer</footer>
-    )
-  }
---
+++ my-app/src/layout/Footer/index.jsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+
+    return (
+      <footer>
+        <ol>
+          <li>Футер</li>
+          <li>Футер</li>
+          <li>Футер</li>
+        </ol>
+      </footer>
+    )
+  }
