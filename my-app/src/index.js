06797a1c39098e673e50b5fe4918708fba9a4c34
anstice <apple@MacBook-Air.local>
2023-05-23 22:55:03 +0300

    создание роутинга

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
