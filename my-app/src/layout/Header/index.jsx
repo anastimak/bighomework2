e87a23522a440933dded2bef0441c83f14071d86
anstice <apple@MacBook-Air.local>
2023-05-24 19:06:17 +0300

    навигация

--- my-app/src/layout/Header/index.jsx
+++ my-app/src/layout/Header/index.jsx
@@ -1,17 +1,37 @@
 import styles from './header.module.css'
 import { Search } from '../../components/Search'
-import { TOKEN } from '../../utils/constants'
+import { AUTH_DOG_TOKEN, TOKEN } from '../../utils/constants'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 export const Header = () => {
+  const navigate = useNavigate()
+  const token = localStorage.getItem(AUTH_DOG_TOKEN)
+
+  const handleExit = () => {
+    localStorage.removeItem(AUTH_DOG_TOKEN)
+    return navigate('signin')
+  }
+
     return (
         <header>
           <div className={styles.logo}><h1>DogFood</h1></div>
           <Search />
     
           <nav>
-            <button class="btn btn-warning">Корзина</button>
-            <button class="btn btn-warning">Избранное</button>
-            <button class="btn btn-warning">Личный кабинет</button>
+            <NavLink to={"/products"}>
+              Каталог
+            </NavLink>
+            <NavLink to={"/cart"}>
+              Корзина
+            </NavLink>
+            <NavLink to={"/user/me"}>
+              User
+            </NavLink>
+            <NavLink to={"/favorites"}>
+              Избранное
+            </NavLink>
+            
+            {token && <button onClick={() => handleExit()}>Выход</button>}
           </nav>
     
         </header>
