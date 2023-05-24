dcc2f6fe5860b4622bceb9aee7ff1195b7186e09
anstice <apple@MacBook-Air.local>
2023-05-24 19:17:32 +0300

    редактирование SignUp

--- my-app/src/pages/SignUp/index.jsx
+++ my-app/src/pages/SignUp/index.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react"
+import { AUTH_DOG_TOKEN } from "../../utils/constants"
+import { useNavigate } from "react-router-dom"
+
 export const SignUp = () => {
- 
+    const navigate = useNavigate()
+    useEffect(() => {
+        const token = localStorage.getItem(AUTH_DOG_TOKEN)
+    
+        if (token) return navigate('/products')
+      }, [navigate])
+    
+
     return(
         <>
         <h1>Регистрация</h1>
