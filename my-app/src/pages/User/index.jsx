0fff8851c6e069fed217a089f70133ccbde0388a
anstice <apple@MacBook-Air.local>
2023-05-24 19:19:07 +0300

    редактирование User

--- my-app/src/pages/User/index.jsx
+++ my-app/src/pages/User/index.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { AUTH_DOG_TOKEN } from "../../utils/constants"
+
 export const User = () => {
  
+    const navigate = useNavigate()
+    useEffect(() => {
+        const token = localStorage.getItem(AUTH_DOG_TOKEN)
+    
+        if (token) return navigate('/products')
+      }, [navigate])
+
     return(
         <>
         <h1>User Me</h1>
